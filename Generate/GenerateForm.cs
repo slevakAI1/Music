70ff1785bdaa49dc4bdcd81ed402ce120f36cafe
Stephen Levak <slevak1@gmail.com>
2025-10-18 19:50:26 -0400

    clean

--- Generate/GenerateForm.cs
+++ Generate/GenerateForm.cs
@@ -26,6 +26,17 @@ namespace Music
             InitializeComponent();
         }
 
+        // Designer is wired to this
+        private void MusicForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Designer may be wired to this
+        private void btnNewScore_Click(object sender, EventArgs e)
+        {
+            btnNewDesign_Click(sender, e);
+        }
+
         private void btnNewDesign_Click(object sender, EventArgs e)
         {
             // Create and persist a new ScoreDesign instance for this form/session
@@ -35,6 +46,22 @@ namespace Music
             txtSongStructure.Clear();
             txtVoiceSet.Clear();
             txtChordSet.Clear();
+
+            // Reset dependent sets and current sections
+            _sections = null;
+            _voiceSet.Reset();
+            _chordSet.Reset();
+        }
+
+        // Designer may be wired to this (alias to CreateSections)
+        private void btnCreateSections_Click(object sender, EventArgs e)
+        {
+            btnCreateScoreStructure_Click(sender, e);
+        }
+
+        private void btnCreateScoreStructure_Click(object sender, EventArgs e)
+        {
+            _sections = _sectionManager.CreateSections(this, txtSongStructure, txtVoiceSet, txtChordSet, _voiceSet, _chordSet);
         }
 
         private void btnAddVoices_Click(object sender, EventArgs e)
@@ -47,9 +74,17 @@ namespace Music
             _chordManager.AddDefaultChords(this, _sections, _chordSet, txtChordSet);
         }
 
-        private void btnCreateScoreStructure_Click(object sender, EventArgs e)
+        // Designer may be wired to this; keep as stub for now
+        private void btnCreateMusic_Click(object sender, EventArgs e)
         {
-            _sections = _sectionManager.CreateSections(this, txtSongStructure, txtVoiceSet, txtChordSet, _voiceSet, _chordSet);
+            // Intentionally left empty until music generation is implemented.
+            // This method exists to satisfy designer wiring.
+        }
+
+        // Designer is wired to this; keep as stub or implement save logic later
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Intentionally left empty until save behavior is defined.
         }
     }
 }
