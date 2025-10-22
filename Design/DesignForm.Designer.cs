790e7bc0527978e8f8b370645f70b2f1b9fb8f9d
Stephen Levak <slevak1@gmail.com>
2025-10-22 18:22:03 -0400

    layout

--- Design/DesignForm.Designer.cs
+++ Design/DesignForm.Designer.cs
@@ -8,29 +8,34 @@ namespace Music
         {
             btnEditSections = new Button();
             btnSave = new Button();
-            btnSelectVoices = new Button();
+            btnEditVoices = new Button();
             btnNew = new Button();
             txtDesignView = new TextBox();
-            btnEditHarmonicTimeline = new Button();
+            btnEditHarmony = new Button();
             btnSetDefault = new Button();
             btnLoad = new Button();
             btnEditTimeSignature = new Button();
             btnEditTempo = new Button();
+            lblEdit = new Label();
+            lblDesign = new Label();
+            label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
             SuspendLayout();
             // 
             // btnEditSections
             // 
-            btnEditSections.Location = new Point(28, 95);
+            btnEditSections.Location = new Point(43, 323);
             btnEditSections.Name = "btnEditSections";
-            btnEditSections.Size = new Size(151, 23);
+            btnEditSections.Size = new Size(96, 23);
             btnEditSections.TabIndex = 0;
-            btnEditSections.Text = "Edit Sections";
+            btnEditSections.Text = "Sections";
             btnEditSections.UseVisualStyleBackColor = true;
             btnEditSections.Click += btnEditSections_Click;
             // 
             // btnSave
             // 
-            btnSave.Location = new Point(42, 317);
+            btnSave.Location = new Point(43, 153);
             btnSave.Name = "btnSave";
             btnSave.Size = new Size(96, 23);
             btnSave.TabIndex = 2;
@@ -38,21 +43,21 @@ namespace Music
             btnSave.UseVisualStyleBackColor = true;
             btnSave.Click += btnSave_Click;
             // 
-            // btnSelectVoices
+            // btnEditVoices
             // 
-            btnSelectVoices.Location = new Point(28, 63);
-            btnSelectVoices.Name = "btnSelectVoices";
-            btnSelectVoices.Size = new Size(151, 23);
-            btnSelectVoices.TabIndex = 4;
-            btnSelectVoices.Text = "Select Voices";
-            btnSelectVoices.UseVisualStyleBackColor = true;
-            btnSelectVoices.Click += btnSelectVoices_Click;
+            btnEditVoices.Location = new Point(43, 278);
+            btnEditVoices.Name = "btnEditVoices";
+            btnEditVoices.Size = new Size(96, 23);
+            btnEditVoices.TabIndex = 4;
+            btnEditVoices.Text = "Voices";
+            btnEditVoices.UseVisualStyleBackColor = true;
+            btnEditVoices.Click += btnSelectVoices_Click;
             // 
             // btnNew
             // 
-            btnNew.Location = new Point(28, 32);
+            btnNew.Location = new Point(43, 120);
             btnNew.Name = "btnNew";
-            btnNew.Size = new Size(151, 23);
+            btnNew.Size = new Size(96, 23);
             btnNew.TabIndex = 13;
             btnNew.Text = "New";
             btnNew.UseVisualStyleBackColor = true;
@@ -60,28 +65,28 @@ namespace Music
             // 
             // txtDesignView
             // 
-            txtDesignView.Location = new Point(280, 12);
+            txtDesignView.Location = new Point(201, 64);
             txtDesignView.Multiline = true;
             txtDesignView.Name = "txtDesignView";
             txtDesignView.ScrollBars = ScrollBars.Vertical;
-            txtDesignView.Size = new Size(775, 631);
+            txtDesignView.Size = new Size(824, 747);
             txtDesignView.TabIndex = 14;
             // 
-            // btnEditHarmonicTimeline
+            // btnEditHarmony
             // 
-            btnEditHarmonicTimeline.Location = new Point(28, 127);
-            btnEditHarmonicTimeline.Name = "btnEditHarmonicTimeline";
-            btnEditHarmonicTimeline.Size = new Size(151, 23);
-            btnEditHarmonicTimeline.TabIndex = 15;
-            btnEditHarmonicTimeline.Text = "Edit Harmonic Timeline";
-            btnEditHarmonicTimeline.UseVisualStyleBackColor = true;
-            btnEditHarmonicTimeline.Click += btnEditHarmonicTimeline_Click;
+            btnEditHarmony.Location = new Point(43, 352);
+            btnEditHarmony.Name = "btnEditHarmony";
+            btnEditHarmony.Size = new Size(96, 23);
+            btnEditHarmony.TabIndex = 15;
+            btnEditHarmony.Text = "Harmony";
+            btnEditHarmony.UseVisualStyleBackColor = true;
+            btnEditHarmony.Click += btnEditHarmonicTimeline_Click;
             // 
             // btnSetDefault
             // 
-            btnSetDefault.Location = new Point(28, 245);
+            btnSetDefault.Location = new Point(43, 88);
             btnSetDefault.Name = "btnSetDefault";
-            btnSetDefault.Size = new Size(151, 23);
+            btnSetDefault.Size = new Size(96, 23);
             btnSetDefault.TabIndex = 16;
             btnSetDefault.Text = "Set Default Timeline";
             btnSetDefault.UseVisualStyleBackColor = true;
@@ -89,7 +94,7 @@ namespace Music
             // 
             // btnLoad
             // 
-            btnLoad.Location = new Point(42, 351);
+            btnLoad.Location = new Point(43, 186);
             btnLoad.Name = "btnLoad";
             btnLoad.Size = new Size(96, 23);
             btnLoad.TabIndex = 17;
@@ -99,37 +104,84 @@ namespace Music
             // 
             // btnEditTimeSignature
             // 
-            btnEditTimeSignature.Location = new Point(28, 162);
+            btnEditTimeSignature.Location = new Point(43, 386);
             btnEditTimeSignature.Name = "btnEditTimeSignature";
-            btnEditTimeSignature.Size = new Size(151, 23);
+            btnEditTimeSignature.Size = new Size(96, 23);
             btnEditTimeSignature.TabIndex = 18;
-            btnEditTimeSignature.Text = "Edit Time Signature";
+            btnEditTimeSignature.Text = "Time Signature";
             btnEditTimeSignature.UseVisualStyleBackColor = true;
             btnEditTimeSignature.Click += btnEditTimeSignature_Click;
             // 
             // btnEditTempo
             // 
-            btnEditTempo.Location = new Point(28, 202);
+            btnEditTempo.Location = new Point(43, 421);
             btnEditTempo.Name = "btnEditTempo";
-            btnEditTempo.Size = new Size(151, 23);
+            btnEditTempo.Size = new Size(96, 23);
             btnEditTempo.TabIndex = 19;
-            btnEditTempo.Text = "Edit Tempo";
+            btnEditTempo.Text = "Tempo";
             btnEditTempo.UseVisualStyleBackColor = true;
             btnEditTempo.Click += btnEditTempo_Click;
             // 
+            // lblEdit
+            // 
+            lblEdit.AutoSize = true;
+            lblEdit.Location = new Point(77, 260);
+            lblEdit.Name = "lblEdit";
+            lblEdit.Size = new Size(27, 15);
+            lblEdit.TabIndex = 20;
+            lblEdit.Text = "Edit";
+            // 
+            // lblDesign
+            // 
+            lblDesign.AutoSize = true;
+            lblDesign.Location = new Point(72, 64);
+            lblDesign.Name = "lblDesign";
+            lblDesign.Size = new Size(43, 15);
+            lblDesign.TabIndex = 21;
+            lblDesign.Text = "Design";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(495, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(127, 47);
+            label1.TabIndex = 22;
+            label1.Text = "Design";
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(28, 73);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(138, 145);
+            panel1.TabIndex = 23;
+            // 
+            // panel2
+            // 
+            panel2.Location = new Point(28, 268);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(138, 194);
+            panel2.TabIndex = 24;
+            // 
             // DesignForm
             // 
-            ClientSize = new Size(1085, 655);
+            ClientSize = new Size(1085, 804);
+            Controls.Add(btnEditVoices);
+            Controls.Add(label1);
+            Controls.Add(lblDesign);
+            Controls.Add(lblEdit);
             Controls.Add(btnEditTempo);
             Controls.Add(btnEditTimeSignature);
             Controls.Add(btnLoad);
             Controls.Add(btnSetDefault);
-            Controls.Add(btnEditHarmonicTimeline);
+            Controls.Add(btnEditHarmony);
             Controls.Add(txtDesignView);
             Controls.Add(btnNew);
-            Controls.Add(btnSelectVoices);
             Controls.Add(btnSave);
             Controls.Add(btnEditSections);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
             Name = "DesignForm";
             WindowState = FormWindowState.Maximized;
             Load += MusicForm_Load;
@@ -138,14 +190,19 @@ namespace Music
         }
         private Button btnEditSections;
         private Button btnSave;
-        private Button btnSelectVoices;
+        private Button btnEditVoices;
         private Button btnAddChords;
         private Button btnNew;
         private TextBox txtDesignView;
-        private Button btnEditHarmonicTimeline;
+        private Button btnEditHarmony;
         private Button btnSetDefault;
         private Button btnLoad;
         private Button btnEditTimeSignature;
         private Button btnEditTempo;
+        private Label lblEdit;
+        private Label lblDesign;
+        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
