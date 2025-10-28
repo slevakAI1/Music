5d8e561afa99b776bec87ece67eed1708f73f50c
Stephen Levak <slevak1@gmail.com>
2025-10-28 14:59:23 -0400

    Added time signature time fields to model

--- MusicXml/Domain/MeasureAttributes.cs
+++ MusicXml/Domain/MeasureAttributes.cs
@@ -12,6 +12,28 @@ namespace MusicXml.Domain
 		public Key Key { get; internal set; }
 		
 		public Time Time { get; internal set; }
+
+		// Proxies to the hierarchical <time> element
+		public int Beats
+		{
+			get => Time?.Beats ?? 0;
+			internal set
+			{
+				if (Time == null) Time = new Time();
+				Time.Beats = value;
+			}
+		}
+
+		// BeatType maps to Time.Mode (MusicXML uses a numeric string here like "4")
+		public string BeatType
+		{
+			get => Time?.Mode;
+			internal set
+			{
+				if (Time == null) Time = new Time();
+				Time.Mode = value;
+			}
+		}
 		 
 		public Clef Clef { get; internal set; }
 	}
