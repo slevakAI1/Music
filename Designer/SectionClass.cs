349605a7fa692957d2743a26359529a6f8f14a86
Stephen Levak <slevak1@gmail.com>
2025-12-11 16:27:59 -0500

    wip

--- Designer/SectionClass.cs
+++ Designer/SectionClass.cs
@@ -21,8 +21,5 @@
 
         // Helper: returns true if this section spans the given bar
         public bool ContainsBar(int bar) => bar >= StartBar && bar < StartBar + BarCount;
-
-        // Helper: 0-based bar index within this section (or -1 if not in this section)
-        public int GetLocalBarIndex(int bar) => ContainsBar(bar) ? (bar - StartBar) : -1;
     }
 }
