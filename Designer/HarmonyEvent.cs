10202fceeb12be9f96134443f33adfc96978b183
Stephen Levak <slevak1@gmail.com>
2025-12-11 22:08:11 -0500

    wip

--- Designer/HarmonyEvent.cs
+++ Designer/HarmonyEvent.cs
@@ -7,21 +7,14 @@ namespace Music.Designer
         public int StartBar { get; init; }
         public int StartBeat { get; init; } = 1;
 
-        // Duration in beats (e.g., 4 for one 4/4 bar)
-        public int DurationBeats { get; init; } = 4;
-
         // Musical properties (kept simple/strings for now)
         public string Key { get; init; } = "C major";
         public int Degree { get; init; } // 1..7
         public string Quality { get; init; } = "maj"; // maj, min7, dom7, etc.
         public string Bass { get; init; } = "root";
 
-        // Returns true if this event is active at the specified bar:beat
-        public bool Contains(int bar, int beat, int beatsPerBar)
-        {
-            var startAbs = (StartBar - 1) * beatsPerBar + (StartBeat - 1);
-            var targetAbs = (bar - 1) * beatsPerBar + (beat - 1);
-            return targetAbs >= startAbs && targetAbs < startAbs + DurationBeats;
-        }
+
+        // For data entry form support only
+        public int DurationBeats { get; init; } = 4;
     }
 }
