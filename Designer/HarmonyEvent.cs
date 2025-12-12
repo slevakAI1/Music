aeb0b48c8f292c9ffd5ae34fe9b4d32c343824a0
Stephen Levak <slevak1@gmail.com>
2025-12-11 22:11:23 -0500

    wip

--- Designer/HarmonyEvent.cs
+++ Designer/HarmonyEvent.cs
@@ -1,6 +1,10 @@
 namespace Music.Designer
 {
     // One harmonic event, potentially spanning multiple bars
+    // A harmonic event is assumed to be active until the next harmonic event starts.
+    // The event time is determined by StartBar and Start Beat. DurationBeats is a user input
+    //      and/or calculated value for use by the Harmonic Event data entry form.
+
     public sealed class HarmonyEvent
     {
         // Placement (1-based bar/beat)
