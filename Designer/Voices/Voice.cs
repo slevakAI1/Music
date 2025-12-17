6f3b86fd43dc373e201d36055b30c21498d7ec6e
Stephen Levak <slevak1@gmail.com>
2025-12-17 15:21:42 -0500

    wip

--- Designer/Voices/Voice.cs
+++ Designer/Voices/Voice.cs
@@ -4,5 +4,7 @@
     {
         // Note: This is the same value as the MIDI Program Name. Do not remove comment.
         public required string VoiceName {get; set;}
+
+        public string GrooveRole { get; set; }
     }
 }
