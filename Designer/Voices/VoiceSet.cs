257dc09660ed28c5a75bc16e81baf63c1691fe8c
Stephen Levak <slevak1@gmail.com>
2025-12-17 15:38:33 -0500

    wip

--- Designer/Voices/VoiceSet.cs
+++ Designer/Voices/VoiceSet.cs
@@ -7,21 +7,21 @@ namespace Music.Designer
 
         public void Reset() => Voices.Clear();
 
-        public void AddVoice(string voiceName)
+        public void AddVoice(string voiceName, string grooveRole = "")
         {
             if (string.IsNullOrWhiteSpace(voiceName))
                 throw new ArgumentException("Voice name must not be null or empty.", nameof(voiceName));
 
-            var voice = new Voice { VoiceName = voiceName };
+            var voice = new Voice { VoiceName = voiceName, GrooveRole = grooveRole };
             Voices.Add(voice);
         }
 
         public IReadOnlyList<Voice> SetTestVoicesD1()
         {
-            AddVoice("Acoustic Grand Piano");
-            AddVoice("Electric Guitar (clean)");
-            AddVoice("Electric Bass (finger)");
-            AddVoice("Drum Set");  // MIDI track 10 reserved for drum set. Does not use Program Number. Then each note is different percussion.
+            AddVoice("Acoustic Grand Piano", "Pads");
+            AddVoice("Electric Guitar (clean)", "Comp");
+            AddVoice("Electric Bass (finger)", "Bass");
+            AddVoice("Drum Set", "DrumKit");  // MIDI track 10 reserved for drum set. Does not use Program Number. Then each note is different percussion.
             return Voices;
         }
     }
