71b40c9d436300221bb9b951f232ea2569f6896d
Stephen Levak <slevak1@gmail.com>
2025-12-19 12:07:16 -0500

    reorg. seems like randomize working but havent stepped thru it yet

--- Designer/Harmony/HarmonyTests.cs
+++ Designer/Harmony/HarmonyTests.cs
@@ -8,7 +8,7 @@ namespace Music.Designer
             var timeline = new HarmonyTimeline();
             timeline.ConfigureGlobal(DesignerTests.GlobalTimeSignature);
 
-            // Common 4-chord loop: I – V – vi – IV, one chord per bar across 48 bars.
+            // Common 4-chord loop: I – V – vi – IV, two bars per chord across 48 bars.
             var pattern = new (int degree, string quality)[]
             {
                 (1, "Major"),
@@ -19,7 +19,7 @@ namespace Music.Designer
 
             for (int bar = 1; bar <= DesignerTests.TotalBars; bar++)
             {
-                var p = pattern[(bar - 1) % pattern.Length];
+                var p = pattern[((bar - 1) / 2) % pattern.Length];
                 Add(timeline, bar, key: "C major", degree: p.degree, quality: p.quality);
             }
 
