ab2f7a392079430fdda6e00bb014ab5da2cef27c
Stephen Levak <slevak1@gmail.com>
2025-12-26 15:56:41 -0500

    wip

--- Generator/SongContext/Timing/TimingTrack.cs
+++ Generator/SongContext/Timing/TimingTrack.cs
@@ -5,7 +5,6 @@ namespace Music.Generator
     public class Timingtrack
     {
         // TO DO SUPER HIGH IMPORTANT!
-        //   THIS HAS THAT OLD barhead code and globals code in it - remove
         //   Add all the start measure ticks, beats per bar, ticksperbar
         //      and any other timing related tick related calcs that are per bar
         //         - calculate and save for every individual measure for ? measures (100?, length of song - may not be known? something else?)
@@ -13,28 +12,14 @@ namespace Music.Generator
         //   Then lookup is just the bar index
         //   Only show one entry in the grid, i.e. dont show the subsequent duplicates - they are hidden!
 
-        private readonly Dictionary<int, TimingEvent> _barHeads = new(); // bar -> event active at beat 1
-
         public int BeatsPerBar { get; set; } = 4;
 
         public List<TimingEvent> Events { get; set; } = new();
-
-        public void ConfigureGlobal(string meter)
-        {
-            // Expect "x/y". For now, only x matters for bar length in beats.
-            if (string.IsNullOrWhiteSpace(meter)) throw new ArgumentException(nameof(meter));
-            var parts = meter.Split('/');
-            if (parts.Length != 2 || !int.TryParse(parts[0], out var beats))
-                throw new ArgumentException("Invalid meter format. Expected like \"4/4\".", nameof(meter));
-
-            BeatsPerBar = Math.Max(1, beats);
-        }
 
         public void Add(TimingEvent evt)
         {
             Events.Add(evt);
             // Clear cache when adding new events
-            _barHeads.Clear();
         }
 
         /// <summary>
