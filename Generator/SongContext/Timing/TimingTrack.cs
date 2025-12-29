0e9889e7ab9767d48c9b51d932ffdb1ce2f001e9
Stephen Levak <sleva@DESKTOP-NGAH9PS>
2025-12-28 21:22:49 -0500

    wip

--- Generator/SongContext/Timing/TimingTrack.cs
+++ Generator/SongContext/Timing/TimingTrack.cs
@@ -4,22 +4,11 @@ namespace Music.Generator
     // Global bar/beat-aligned time signature track
     public class Timingtrack
     {
-        // TO DO SUPER HIGH IMPORTANT!
-        //   Add all the start measure ticks, beats per bar, ticksperbar
-        //      and any other timing related tick related calcs that are per bar
-        //         - calculate and save for every individual measure for ? measures (100?, length of song - may not be known? something else?)
-        //          must do here because this is where the time signatures are edited
-        //   Then lookup is just the bar index
-        //   Only show one entry in the grid, i.e. dont show the subsequent duplicates - they are hidden!
-
-        public int BeatsPerBar { get; set; } = 4;
-
         public List<TimingEvent> Events { get; set; } = new();
 
         public void Add(TimingEvent evt)
         {
             Events.Add(evt);
-            // Clear cache when adding new events
         }
 
         /// <summary>
